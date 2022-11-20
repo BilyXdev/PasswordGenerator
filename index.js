84de7dff8b61329a50c18e7cbed652bd92d2f4f7
Bily <117677485+BilyXdev@users.noreply.github.com>
2022-11-20 16:30:48 +0100

    Update index.js
    I add the JS part of the "Change the number of characters" functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordLength = 15
+let passwordLengthRender = "Password length: "
+
 let genPasswordOne = document.querySelector("#gen-password-one")
 let genPasswordTwo = document.querySelector("#gen-password-two")
 
@@ -18,3 +21,19 @@ function resetOnClick() {
     genPasswordOne.textContent = ""
     genPasswordTwo.textContent = ""
 }
+
+let removeLengthBtn = document.querySelector("#remove-length-btn")
+let addLengthBtn = document.querySelector("#add-length-btn")
+
+removeLengthBtn.addEventListener("click" , function () {
+    passwordLength -= 1
+    lengthRender.textContent = passwordLengthRender + passwordLength
+
+})
+addLengthBtn.addEventListener("click" , function () {
+    passwordLength += 1
+    lengthRender.textContent = passwordLengthRender + passwordLength
+})
+
+let lengthRender = document.querySelector("#password-length-render")
+lengthRender.textContent = passwordLengthRender + passwordLength
